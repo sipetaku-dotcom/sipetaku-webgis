9c3411939440a0ddfa9f01a0b142605062e22c59
Ricky Hermawan <sipetaku@gmail.com>
2026-05-21 14:27:22 +0800

    Use SQLite for demo deployment

--- config/settings.py
+++ config/settings.py
@@ -96,12 +96,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sipetaku_db',
-        'USER': 'postgres',
-        'PASSWORD': 'admin123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
